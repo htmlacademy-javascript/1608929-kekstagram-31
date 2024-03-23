8c65caa42666901f6a765f06abf1e7331aab5c40
Анастасия <kamanina963@gmail.com>
2024-03-23 20:26:47 +0300

    Создай функцию для проверки палиндрома

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,14 @@ const isStringLess = (checkingString, numberOfCharacters) => {
 
   return false;
 };
+
+const isPalindrome = (checkingString) => {
+  const amountCharacters = checkingString.length;
+  let newCheckingString = '';
+
+  for (let i = amountCharacters - 1; i >= 0; i--) {
+    newCheckingString += checkingString[i];
+  }
+
+  return checkingString.toUpperCase().replace(/ /g, '') === newCheckingString.toUpperCase().replace(/ /g, '');
+};
