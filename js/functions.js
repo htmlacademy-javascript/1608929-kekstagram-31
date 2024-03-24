82a8c62eee7d3532fbbe3d4c5398f7e74cca4554
Анастасия <kamanina963@gmail.com>
2024-03-24 14:53:41 +0300

    Создай функцию извлекающую положительное число

--- js/functions.js
+++ js/functions.js
@@ -6,13 +6,38 @@ const isStringLess = (checkingString, numberOfCharacters) => {
   return false;
 };
 
-const isPalindrome = (checkingString) => {
-  const amountCharacters = checkingString.length;
-  let newCheckingString = '';
+const isPalindrome = (userString) => {
+  const amountCharacters = userString.length;
+  let newUserString = '';
 
   for (let i = amountCharacters - 1; i >= 0; i--) {
-    newCheckingString += checkingString[i];
+    newUserString += userString[i];
   }
 
-  return checkingString.toUpperCase().replace(/ /g, '') === newCheckingString.toUpperCase().replace(/ /g, '');
+  return userString.toUpperCase().replace(/ /g, '') === newUserString.toUpperCase().replace(/ /g, '');
+};
+
+const getNumbers = (userString) => {
+  if (!isNaN(userString)) {
+    if (Number.isInteger(userString) && userString > 0) {
+      return userString;
+    }
+
+    return NaN;
+  } else {
+    let newUserString = '';
+    const withoutSpacesUserString = userString.replace(/ /g, '');
+
+    for (let i = 0; i <= withoutSpacesUserString.length - 1; i++) {
+      newUserString += (!isNaN(withoutSpacesUserString[i])) ? withoutSpacesUserString[i] : '' ;
+    }
+
+    if (Number.isInteger(userString) && userString > 0) {
+      return userString;
+    }
+
+    const numberUserString = Number(newUserString)
+
+    return numberUserString && numberUserString > 0 ? numberUserString : NaN;
+  }
 };
